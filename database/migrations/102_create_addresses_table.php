4786794f387e083d9cb322f70a0c2aa72faa794d
Jorge Omner Arias Olea~ <omnerarias001@gmail.com>
2025-03-25 21:13:11 -0600

    cracion de los modelos y de las tablas

---
+++ database/migrations/102_create_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string("line_1")->nullable()->default(null);
+            $table->string("line_2")->nullable()->default(null);
+            $table->string("city")->nullable()->default(null);
+            $table->string("state")->nullable()->default(null);
+            $table->string("zip")->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
